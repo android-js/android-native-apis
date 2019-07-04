0326aeb8cc004ae5a2ced38efa56adec2d87d290
Chhekur <820121223505e@gmail.com>
2019-07-04 22:12:16 +0530

    Fixed: Contact API

--- androidjs/src/main/java/com/android/js/api/Contact.java
+++ androidjs/src/main/java/com/android/js/api/Contact.java
@@ -40,9 +40,16 @@ public class Contact extends ReactContextBaseJavaModule {
                 JSONObject contact = new JSONObject();
                 contact.put("name", this.cursor.getString(this.cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
                 contact.put("phone_number", this.cursor.getString(this.cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
-                contact.put("email", this.cursor.getString(this.cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA)));
-                contact.put("company", this.cursor.getString(this.cursor.getColumnIndex(ContactsContract.CommonDataKinds.Organization.COMPANY)));
-                contact.put("job_title", this.cursor.getString(this.cursor.getColumnIndex(ContactsContract.CommonDataKinds.Organization.TITLE)));
+
+                String id = this.cursor.getString(this.cursor.getColumnIndex(ContactsContract.Contacts._ID));
+
+                Cursor ce = this.activity.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?", new String[]{id}, null);
+
+                if(ce != null && ce.moveToFirst()) {
+                    contact.put("email", ce.getString(ce.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA)));
+                    ce.close();
+                }
                 this.contacts.put(contact);
             }
             this.cursor.close();
@@ -51,7 +58,7 @@ public class Contact extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void addContact(String name, String number, String email, String company, String job_title) throws OperationApplicationException, RemoteException {
+    public String addContact(String name, String number, String email){
         ArrayList< ContentProviderOperation > ops = new ArrayList <ContentProviderOperation> ();
         ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
                 .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
@@ -81,25 +88,21 @@ public class Contact extends ReactContextBaseJavaModule {
                     .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
                     .build());
         }
-        if (company != null && job_title != null) {
-            ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                    .withValue(ContactsContract.Data.MIMETYPE,
-                            ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
-                    .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, company)
-                    .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
-                    .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, job_title)
-                    .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
-                    .build());
+
+        try {
+            this.activity.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+            return "{error:false, msg:'contact added'}";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "{error:true, err: '"+ e.toString() + "'}";
         }
-        this.activity.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
     public String getContactByName(String name) throws JSONException{
         if (this.contacts.length() == 0) this.getAllContacts();
         for(int i = 0; i < this.contacts.length(); i++){
-            if(this.contacts.getJSONObject(i).has(name)) return this.contacts.getJSONObject(i).toString();
+            if(this.contacts.getJSONObject(i).getString("name").equals(name)) return this.contacts.getJSONObject(i).toString();
         }
         return "Not Found";
     }
