7fdce10a2c602fcd638e402c0cc57171e3b89b64
Paweł Suwiński <dracono@wp.pl>
2020-12-07 14:48:41 +0100

    app.exec() implementation

--- androidjs/src/main/java/com/android/js/api/App.java
+++ androidjs/src/main/java/com/android/js/api/App.java
@@ -3,6 +3,13 @@ package com.android.js.api;
 import android.app.Activity;
 import android.os.Environment;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 import static android.os.Environment.DIRECTORY_ALARMS;
 import static android.os.Environment.DIRECTORY_DCIM;
 import static android.os.Environment.DIRECTORY_DOWNLOADS;
@@ -12,6 +19,8 @@ import static android.os.Environment.DIRECTORY_NOTIFICATIONS;
 import static android.os.Environment.DIRECTORY_PICTURES;
 import static android.os.Environment.DIRECTORY_PODCASTS;
 import static android.os.Environment.DIRECTORY_RINGTONES;
+
+import static com.android.js.other.Utils.exec;
 
 public class App {
     private Activity activity;
@@ -54,5 +63,30 @@ public class App {
         } else {
             return "-1";
         }
+    }
+
+    public static Map<String, String> exec(String[] cmdarray) { 
+        Map<String, String> result = new HashMap<String, String>();
+        try { 
+            Process process = Runtime.getRuntime().exec(cmdarray);
+            process.waitFor();
+            result.put("status", String.valueOf(process.exitValue()));
+            result.put("stdout", readStream(process.getInputStream()));
+            result.put("stderr", readStream(process.getErrorStream()));
+        } catch (Exception e) {
+            result.put("error", e.getMessage());
+        }
+        return result;
+    }
+
+    private static String readStream(InputStream stream) throws IOException {
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(stream));
+        StringBuilder content = new StringBuilder(); 
+        String s;
+        while((s = reader.readLine()) != null) {
+            content.append(s + "\n");
+        }
+        return content.toString();
     }
 }
